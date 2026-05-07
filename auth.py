2946213de0c6843f4f54ab6098ef73b3f5a84d33
Mehmet Cam <73923157+mehmetcamofficial@users.noreply.github.com>
2026-05-07 22:13:08 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,26 @@
 import streamlit as st
+from db import check_login, create_user
 
 def login():
+
     st.sidebar.title("🔐 Login")
 
     username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
 
-    if username:
-        st.session_state["user"] = username
-        return username
+    mode = st.sidebar.radio("Mode", ["Login", "Register"])
+
+    if username and password:
+
+        if mode == "Register":
+            create_user(username, password)
+            st.sidebar.success("Account created")
+
+        if mode == "Login":
+            if check_login(username, password):
+                st.session_state["user"] = username
+                return username
+            else:
+                st.sidebar.error("Invalid credentials")
 
     return None
